e8f307dfea3d1c542c497086253674deb14366c8
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-27 20:56:02 +0545

    png to jpg

--- Day4/03_QRCodeGen/end/qr_gen.py
+++ Day4/03_QRCodeGen/end/qr_gen.py
@@ -13,13 +13,13 @@ output_file = 'qrcode.png'
 # QRCode function takes various arguments which are useful for generating
 # qr code like the version to specify the size of qr code, error correction
 # method, size of each box in pixels, border size in boxes.
+
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-
 # Add the data in the qr.
 qr.add_data(data)
 qr.make(fit=True)
--- Day4/04_ExtConversion/start/ext_convert.py
+++ Day4/04_ExtConversion/start/ext_convert.py
@@ -1,3 +1,32 @@
 """
 Convert all the png images to jpg in a given source folder and store the converted image to the target folder.
 """
+import os 
+from PIL import Image
+
+
+
+
+source_folder = './source_images'
+target_folder = './target_images'
+
+if not os.path.exists(target_folder):
+  os.makedirs(target_folder)
+
+images = os.listdir(source_folder)
+
+for image in images:
+  if image.endswith('.png'):
+    base=os.path.splitext(image)[0]
+
+    new_name = base + '.jpg'
+    
+    img_path = os.path.join(source_folder, image)
+
+    img = Image.open(img_path).convert('RGB')
+
+    dest_img_path = os.path.join(target_folder, new_name)
+
+    img.save(dest_img_path)
+
+print("extension conversion completed")
