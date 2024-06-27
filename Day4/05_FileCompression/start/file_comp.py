5c008ab67bdcbbdfdafb86db8cc3994bd9583b09
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-27 21:00:47 +0545

    file compression

--- Day4/05_FileCompression/start/file_comp.py
+++ Day4/05_FileCompression/start/file_comp.py
@@ -1,3 +1,20 @@
 """
 Compress the given file using gzip.
 """
+import os 
+import shutil
+import gzip
+
+input_file_path = '../../04_ExtConversion/end/ext_convert.py'
+output_file_path = 'ext_convert.py.gz'
+
+with open (input_file_path, 'rb') as f_in:
+  with gzip.open(output_file_path, 'wb', compresslevel=9) as f_out:
+    shutil.copyfileobj(f_in, f_out)
+
+in_size = os.path.getsize(input_file_path)
+out_size = os.path.getsize(output_file_path)
+
+
+print(f"Input file size: {in_size}")
+print(f"Output file size: {out_size}")
